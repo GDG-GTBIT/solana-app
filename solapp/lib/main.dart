699c58001cc063a57f2c749bb278025e9f7c2448
Daksh2356 <dakshmakhija@gmail.com>
2022-09-06 14:22:08 +0530

    Removed debug mode banner

--- solapp/lib/main.dart
+++ solapp/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
